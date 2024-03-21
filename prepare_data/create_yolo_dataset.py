aaf345afc9ce8f9d983519c7834a6861c3f292ad
Amirbahador Moineddini <abmoineddini@gmail.com>
2024-03-21 11:46:10 +0000

    Preprocessing done

--- prepare_data/create_yolo_dataset.py
+++ prepare_data/create_yolo_dataset.py
@@ -17,6 +17,7 @@ for set_ in ['train', 'validation', 'test']:
         os.mkdir(dir_)
 
 Tennis_ball_id = '/m/05ctyq'
+class_names = 'tennis_ball'
 
 train_bboxes_filename = os.path.join('.', 'data_all/boxes/oidv6-train-annotations-bbox.csv')
 validation_bboxes_filename = os.path.join('.', 'data_all/boxes/validation-annotations-bbox.csv')
@@ -25,6 +26,8 @@ test_bboxes_filename = os.path.join('.', 'data_all/boxes/test-annotations-bbox.c
 
 for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename, test_bboxes_filename]):
     set_ = ['train', 'validation', 'test'][j]
+    print(j)
+    print(set_)
     print(filename)
     with open(filename, 'r') as f:
         line = f.readline()
@@ -32,7 +35,7 @@ for j, filename in enumerate([train_bboxes_filename, validation_bboxes_filename,
             id, _, class_name, _, x1, x2, y1, y2, _, _, _, _, _ = line.split(',')[:13]
             if class_name in [Tennis_ball_id]:
                 if not os.path.exists(os.path.join(DATA_OUT_DIR, set_, 'imgs', f'{id}.jpg')):
-                    shutil.copy(os.path.join(DATA_ALL_DIR,set_, f'{id}.jpg'),
+                    shutil.copy(os.path.join(DATA_ALL_DIR,set_, class_names,f'{id}.jpg'),
                                 os.path.join(DATA_OUT_DIR, set_, 'imgs', f'{id}.jpg'))
                 with open(os.path.join(DATA_OUT_DIR, set_, 'anns', f'{id}.txt'), 'a') as f_ann:
                     # class_id, xc, yx, w, h
